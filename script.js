9a27412f4f156a4cad34b25f40032a3c751f26c5
Onno de Jong <onnodejong@live.nl>
2019-04-09 16:19:41 +0200

    lesson 1 code

---
+++ script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+window.onload = function() {
+    createCard('2');
+    createCard('3');
+    createCard('4');
+    createCard('5');
+    createCard('6');
+    createCard('7');
+    createCard('8');
+    createCard('9');
+    createCard('10');
+    createCard('B');
+    createCard('Q');
+    createCard('K');
+    createCard('ACE');
+};
+
+function createCard(name) {
+  const cardContainer = document.getElementById('card-container');
+  const card = document.createElement('div');
+  card.innerHTML = `<div class="card"><b>${name}</b></div>`;
+  cardContainer.appendChild(card);
+  card.onclick = function() {
+    console.log('click', name);
+  };
+}
